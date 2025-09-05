cc018a61a79e043dbc36d806e2462c8b6210b7e9
PixelFritz <twi.wonder@gmail.com>
2025-09-05 20:56:00 -0300

    update: desafio 2 - criar a função Fibonacci() e mostrar o resultado do n-ésimo número

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,14 +2,23 @@
 // Convenção de mercado: colocar em cmd/<nome-app>/main.go
 package main
 
-// Importa os pacotes necessários 
+// Importa os pacotes necessários
 import (
-    "fmt"
-    "github.com/seu-usuario/meu-projeto-go/internal/hello"
+	"fmt"
+	"os"
+
+	"github.com/seu-usuario/meu-projeto-go/internal/fibonacci"
+	"github.com/seu-usuario/meu-projeto-go/internal/hello"
 )
 
 // Função principal do programa
 func main() {
     fmt.Println("🚀 Meu primeiro projeto em Go com estrutura de mercado!")
     hello.SayHello()
+
+    var entradaFibonacci int
+    fmt.Print("\n\nEscolha um número para calcular o Fibonacci: ")
+	fmt.Fscan(os.Stdin, &entradaFibonacci)
+    result := fibonacci.Fibonacci(entradaFibonacci)
+	fmt.Printf("O resultado do Fibonacci(%v) é: %v\n", entradaFibonacci, result)
 }
