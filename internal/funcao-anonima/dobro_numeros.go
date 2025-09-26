aa7505f1b582a1eb4b2acdbbc5197f936e47ae07
PixelFritz <twi.wonder@gmail.com>
2025-09-26 19:59:54 -0300

    UPDATE: Explicação Funções Anônimas

--- internal/funcao-anonima/dobro_numeros.go
+++ internal/funcao-anonima/dobro_numeros.go
@@ -2,6 +2,8 @@ package funcaoanonima
 
 import "fmt"
 
+// Funções anônimas são funções não nomeadas, geralmente utilizadas para coisas rápidas, como callbacks e expressões simples. São utilizadas geralmente dentro do mesmo arquivo e as variáveis podem receber outros valores.
+
 var numeros = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
 var dobrar = func(x int) int {
